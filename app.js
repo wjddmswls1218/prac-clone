172cdcfb716843b4f429b814a70ffba09462c762
wjddmswls1218 <ouhouh2222@gmail.com>
2021-11-15 14:18:11 +0900

    A

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 const db = require("./db");
 const session = require("express-session");
+const globalRouter = require("./routers/globalRouter");
 
 dotenv.config();
 
